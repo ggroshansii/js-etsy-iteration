5038c21f1d2962d77284de284fb02362025eacd4
ggroshansii <garthgroshansii@gmail.com>
2021-09-08 14:40:18 -0400

    commented out console.logs so I can go back and try to refactor with lodash one by one

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ let average = (items.reduce((accum, element) => {
     return accum + element.price;
 },0) / items.length).toFixed(2);
 
-console.log(`$${average}`);
+//console.log(`$${average}`);
 
 //////////////ARRAY OF ITEMS THAT COST BETWEEN $14-$18//////////
 
@@ -17,14 +17,14 @@ let specificItems = items.filter(element => {
     return element.price > 14 && element.price < 18;
 });
 
-console.log(specificItems);
+//console.log(specificItems);
 
 ///////////////////GBP currency code items////////////////
 let itemsGBP = items.filter(element => {
     return element.currency_code === "GBP"
 });
 
-console.log(`${itemsGBP[0].title} costs ${itemsGBP[0].price}`)
+//console.log(`${itemsGBP[0].title} costs ${itemsGBP[0].price}`)
 
 ////////////////Items made of wood///////////////////////
 
@@ -32,18 +32,18 @@ let woodenItems = items.filter(element => {
     return element.materials.includes('wood');
     //return element.title;
 }).forEach(element => {
-    console.log(`${element.title} is made of wood`)
+    //console.log(`${element.title} is made of wood`)
 });
 
 //////////////////Items with 8 or more materials///////////////////
 
 let eightOrMore = items.filter(element => {
-    console.log
+
     return element.materials.length >= 8;
 }).forEach(element => {
-    console.log(`${element.title} has ${element.materials.length} materials`);
+    //console.log(`${element.title} has ${element.materials.length} materials`);
     element.materials.forEach(element => {
-        console.log(`${element}`)
+        //console.log(`${element}`)
     });
 });
 
@@ -56,5 +56,5 @@ let madeBySellers = items.reduce((accum, element) => {
     return accum;
 }, 0);
 
-console.log(`${madeBySellers} were made by their sellers`);
+//console.log(`${madeBySellers} were made by their sellers`);
 
